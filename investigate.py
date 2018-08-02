fa41a97a12dc6f859f284b744650a75f6a5661c5
Austin Hobbs <austin.hobbs@microsoft.com>
2018-08-02 12:16:09 -0500

    gate

--- investigate.py
+++ investigate.py
@@ -1,4 +1,11 @@
 import sys
+import os
+import re
 
-with open('/tmp/test', 'w') as f:
-    f.writelines(sys.version)
+# with open('/tmp/test', 'w') as f:
+#     f.writelines(sys.version)
+
+sdc_devices = sorted([d for d in os.listdir('/dev') if re.match(r'sdc\d+', d)])
+
+for sdc in sdc_devices:
+    os.mkdir(os.path.join('/mnt', sdc))
