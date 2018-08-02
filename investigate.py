1544e5aa2b7984cf04a095720745687d8abcd5e8
Austin Hobbs <austin.hobbs@microsoft.com>
2018-08-02 12:25:57 -0500

    gate

--- investigate.py
+++ investigate.py
@@ -1,11 +1,16 @@
 import sys
 import os
 import re
-
+import subprocess
 # with open('/tmp/test', 'w') as f:
 #     f.writelines(sys.version)
 
 sdc_devices = sorted([d for d in os.listdir('/dev') if re.match(r'sdc\d+', d)])
 
 for sdc in sdc_devices:
-    os.mkdir(os.path.join('/mnt', sdc))
+    mnt_path = os.path.join('/mnt', sdc)
+    os.mkdir(mnt_path)
+
+    if not os.path.ismount(sdc):
+        subprocess.call(['mount', os.path.join('/dev', sdc), mnt_path])
+
