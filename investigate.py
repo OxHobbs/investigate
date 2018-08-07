daabceb2ca532bab6956c0cb63f632f5743947e6
Austin Hobbs <austin.hobbs@microsoft.com>
2018-08-06 19:43:54 -0500

    added azure agent log

--- investigate.py
+++ investigate.py
@@ -13,9 +13,7 @@
 v0.2.0
 """
 
-
 import argparse
-import sys
 import os
 import re
 import subprocess
@@ -102,11 +100,11 @@ def get_kernels_in_boot(boot_path):
         kernel_ctime = os.path.getctime(kernel_path)
         kernel_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(kernel_ctime))
         kernel_dict_list.append({
-                'kernel_name': kernel,
-                'creation_date': kernel_time})
+            'kernel_name': kernel,
+            'creation_date': kernel_time})
 
     sorted_kernel_list = sorted(kernel_dict_list, key=lambda x: x['creation_date'], reverse=True)
-    
+
     return sorted_kernel_list
 
 
@@ -125,7 +123,8 @@ def create_storage_container(block_blob_service):
     subject_container_name = get_subject_container_name()
 
     if subject_container_name not in containers:
-        print('Creating container {} in storage account {}'.format(subject_container_name, block_blob_service.account_name))
+        print('Creating container {} in storage account {}'.format(subject_container_name,
+                                                                   block_blob_service.account_name))
         block_blob_service.create_container(subject_container_name)
     else:
         print('Container: {} already exists'.format(subject_container_name))
@@ -151,20 +150,47 @@ def copy_grub_cfg_files(boot_sdc):
         shutil.copyfile(full_path, os.path.join('/tmp', 'grub', gfile))
 
 
+def get_azure_log_tmp():
+    return os.path.join('/tmp', 'azure_logs', 'waagent.log')
+
+
+def find_azure_log(sdc_devices):
+    for sdc in sdc_devices:
+        mnt_path = os.path.join('/mnt', sdc)
+        azure_log_temp = os.path.join(mnt_path, 'var', 'log', 'waagent.log')
+
+        if os.path.exists(azure_log_temp):
+            return azure_log_temp
+
+
+def copy_azure_log_to_tmp(azure_log):
+    azure_tmp_full = get_azure_log_tmp()
+    azure_tmp_dir, _ = os.path.split(azure_tmp_full)
+
+    if not os.path.exists(azure_tmp_dir):
+        os.mkdir(azure_tmp_dir)
+
+    shutil.copyfile(azure_log, azure_tmp_full)
+
+
 def get_blobs_to_upload():
     fdate = dt.utcnow().strftime('%Y%m%d')
     blob_list = [{
-        'blob_name': "messages-gzip-{}".format(fdate),
-        'blob_path': get_messages_archive_path()
-    },
-    {
-        'blob_name': "kernel-list-{}".format(fdate),
-        'blob_path': get_kernels_archive_path()
-    },
-    {
-        'blob_name': "messages-txt-{}".format(fdate),
-        'blob_path': get_messages_tmp_path()
-    }]
+            'blob_name': "messages-gzip-{}".format(fdate),
+            'blob_path': get_messages_archive_path()
+        },
+        {
+            'blob_name': "kernel-list-{}".format(fdate),
+            'blob_path': get_kernels_archive_path()
+        },
+        {
+            'blob_name': "messages-txt-{}".format(fdate),
+            'blob_path': get_messages_tmp_path()
+        },
+        {
+            'blob_name': "waagent-log-{}".format(fdate),
+            'blob_path': get_azure_log_tmp()
+        }]
 
     grub_tmp_files = get_grub_tmp_paths()
 
@@ -204,35 +230,36 @@ def get_endpoint_suffix(cloud):
         return 'core.usgovcloudapi.net'
     elif cloud.lower() == 'azurecloud':
         return 'core.windows.net'
-    
+
     return None
 
 
 def pargs():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--storage-account-name', help='Provide the name of the storage account', required=True)
-    parser.add_argument('-k', '--key', help='Proivde the storage account key', required=True)
+    parser.add_argument('-k', '--key', help='Provide the storage account key', required=True)
     parser.add_argument('-c', '--cloud',
-        help='Provide the Cloud Environment of the storage account',
-        choices=('azureusgovernment', 'azurecloud'),
-        type=str.lower,
-        default='AzureCloud')
+                        help='Provide the Cloud Environment of the storage account',
+                        choices=('azureusgovernment', 'azurecloud'),
+                        type=str.lower,
+                        default='AzureCloud')
     parser.add_argument('-v', '--verbose', action='store_true', help='Get verbose output from the cmd')
-    
+
     args = parser.parse_args()
     sa_name = args.storage_account_name
     sa_key = args.key
     cloud = args.cloud
     verbose = args.verbose
-    
+
     return sa_name, sa_key, cloud, verbose
+
 
 def print_verbose(enabled, msg):
     if enabled:
         print(msg)
 
 
-def main():    
+def main():
     sa_name, sa_key, cloud, verbose = pargs()
     cloud_endpoint = get_endpoint_suffix(cloud)
 
@@ -243,7 +270,6 @@ def main():
         verbose
     ))
     print_verbose(verbose, "Will use storage endpoint suffix: {}".format(cloud_endpoint))
-
 
     sdc_devices = get_sdc_devices()
     print_verbose(verbose, "Found {} sdc devices: {}".format(len(sdc_devices), sdc_devices))
@@ -259,11 +285,14 @@ def main():
 
     copy_grub_cfg_files(boot_sdc)
 
+    az_log = find_azure_log(sdc_devices)
+    copy_azure_log_to_tmp(az_log)
+
     block_blob_service = BlockBlobService(account_name=sa_name, account_key=sa_key, endpoint_suffix=cloud_endpoint)
 
     print_verbose(verbose, "Creating the storage container: {}".format(get_subject_container_name()))
     create_storage_container(block_blob_service)
-    print_verbose(verbose, "Uploding blobs to container")
+    print_verbose(verbose, "Uploading blobs to container")
 
     upload_blobs(block_blob_service)
 
